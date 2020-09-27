c03513b6ab1a4654ba5bd9b76771aef88ef5b217
Aditya Kumar Singh <adityasingh3007@gmail.com>
2020-09-27 21:21:16 +0530

    Basic fixed prog. bootloader code added

---
+++ Code/Blink/Blink.ino
@@ -0,0 +1,13 @@
+#include<avr/io.h>
+#include<util/delay.h>
+int main(void){
+  DDRB = 0xFF;
+  PORTB = 0x00;
+  while(1){
+    PORTB = 0xFF;
+    _delay_ms(1000);
+    PORTB = 0x00;
+    _delay_ms(1000);
+  }
+  return 0;
+}
