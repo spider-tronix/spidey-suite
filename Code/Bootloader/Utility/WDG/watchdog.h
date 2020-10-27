9f5e89e3c9221a8d8a176cb84321f549026c035c
Aditya Kumar Singh <adityasingh3007@gmail.com>
2020-10-27 19:10:04 +0530

    WATCHDOG timer code documented.

--- Code/Bootloader/Utility/WDG/watchdog.h
+++ Code/Bootloader/Utility/WDG/watchdog.h
@@ -1,7 +1,10 @@
+/*
+ * watchdog.h
+ * Created: 10/10/2020 16:09:09 Hrs
+ * Author: Aditya Kumar Singh
+*/
 #ifndef watchdog_h
 #define watchdog_h
-
-//#include<avr/io.h>
 
 /* Watchdog Timer settings */
 #define WATCHDOG_OFF    (0)
@@ -14,24 +17,15 @@
 #define WATCHDOG_1S     (_BV(WDP2) | _BV(WDP1) | _BV(WDE))
 #define WATCHDOG_2S     (_BV(WDP2) | _BV(WDP1) | _BV(WDP0) | _BV(WDE))
 #ifdef  WDP3
-  #define WATCHDOG_4S     (_BV(WDP3) | _BV(WDE))
-  #define WATCHDOG_8S     (_BV(WDP3) | _BV(WDP0) | _BV(WDE))
+#define WATCHDOG_4S     (_BV(WDP3) | _BV(WDE))
+#define WATCHDOG_8S     (_BV(WDP3) | _BV(WDP0) | _BV(WDE))
 #endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-/* Function to reset the watchdog timer */
-void WATCHDOG_RST();
-#ifdef __cplusplus
-}
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-/* Function to configure the watchdog timer settings */
-void WATCHDOG_CONFIG(uint8_t);
+void WATCHDOG_RST();  /* Function to reset the watchdog timer */
+void WATCHDOG_CONFIG(uint8_t); /* Function to configure the watchdog timer settings */
 #ifdef __cplusplus
 }
 #endif
